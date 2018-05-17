0987241a2abfe3cd617e2b993c5809aa5915b1dd
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-05-17 13:00:20 +0300

    Fix tab usage.

--- htdocs/wp-content/themes/syopajatyo/resources/views/content/single.php
+++ htdocs/wp-content/themes/syopajatyo/resources/views/content/single.php
@@ -9,18 +9,18 @@
 <div class="px-2 py-8">
 	<div class="grid mx-auto max-width-1">
 		<main id="main" class="app-main grid__span-2">
-			<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post();
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
 
-					Hybrid\render_view( 'entry/single', Hybrid\get_post_hierarchy() );
+				Hybrid\render_view( 'entry/single', Hybrid\get_post_hierarchy() );
 				endwhile;
 
-				comments_template();
+			comments_template();
 
 			endif;
-			?>
+		?>
 		</main>
 
 		<?php
