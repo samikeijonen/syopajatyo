a316f9d61eb0e3c3d90eb5ae6fd39f534dd0265e
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-05-18 09:35:53 +0300

    Show byline only on posts.

--- htdocs/wp-content/themes/syopajatyo/resources/views/partials/byline.php
+++ htdocs/wp-content/themes/syopajatyo/resources/views/partials/byline.php
@@ -5,6 +5,9 @@
  * @package Syopajatyo
  */
 
+if ( 'post' !== get_post_type() ) :
+	return;
+endif;
 ?>
 <div class="entry__byline font-size-875 <?= esc_attr( $data->class ) ?>">
 	<?php Hybrid\post_date(); ?>
