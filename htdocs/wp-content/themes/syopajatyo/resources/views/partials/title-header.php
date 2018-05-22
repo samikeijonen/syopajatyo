10cddc875b82e066632308e5af97085bcad1095b
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-05-22 14:10:04 +0300

    Add region title.

--- htdocs/wp-content/themes/syopajatyo/resources/views/partials/title-header.php
+++ htdocs/wp-content/themes/syopajatyo/resources/views/partials/title-header.php
@@ -5,20 +5,23 @@
  * @package Syopajatyo
  */
 
+if ( is_front_page() ) :
+	return;
+endif;
 ?>
 <section class="title-header px-2 py-4" role="region" aria-labelledby="region-title">
 	<div class="mx-auto max-width-1">
 		<?php
 		if ( is_singular() || is_home() ) :
 		?>
-			<h1 class="title-header__title mb-0"><?php single_post_title(); ?></h1>
+			<h1 class="title-header__title mb-0" id="region-title"><?php single_post_title(); ?></h1>
 		<?php
 		elseif ( is_archive() ) :
-			the_archive_title( '<h1 id="region-title" class="archive-header__title mb-0">', '</h1>' );
+			the_archive_title( '<h1 class="archive-header__title mb-0" id="region-title">', '</h1>' );
 			the_archive_description( '<div class="archive-header__description">', '</div>' );
 		elseif ( is_search() ) :
 			/* translators: %s: search query. */
-			printf( '<h1 class="title-header__title mb-0">' . esc_html__( 'Search Results for: %s', 'syopajatyo' ) . '</h1>', '<span>' . get_search_query() . '</span>' );
+			printf( '<h1 class="title-header__title mb-0" id="region-title">' . esc_html__( 'Search Results for: %s', 'syopajatyo' ) . '</h1>', '<span>' . get_search_query() . '</span>' );
 		endif;
 		?>
 	</div>
