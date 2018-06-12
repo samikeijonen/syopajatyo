e4b7578cccc7647efeb6f196b4d662b5ba05c78b
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-12 09:34:53 +0300

    Add link to events page.

--- htdocs/wp-content/themes/syopajatyo/resources/views/entry/archive/card.php
+++ htdocs/wp-content/themes/syopajatyo/resources/views/entry/archive/card.php
@@ -11,9 +11,13 @@ $syopajatyo_bg = Syopajatyo\get_post_thumbnail_bg();
 <article <?php Hybrid\attr( 'entry', 'card' ); ?><?= wp_kses_post( $syopajatyo_bg ); ?>>
 	<header class="entry__header">
 		<?php
-		Hybrid\post_terms( [
-			'taxonomy' => 'category',
-		] );
+		if ( 'tribe_events' === get_post_type() ) :
+			echo '<span class="entry__terms"><a href="/tapahtumat/">' . esc_html__( 'Events', 'syopajatyo' ) . '</a></span>';
+		else :
+			Hybrid\post_terms( [
+				'taxonomy' => 'category',
+			] );
+		endif;
 		?>
 		<h2 class="entry__title h3"><a class="decoration-none h-decoration-underline color-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	</header>
