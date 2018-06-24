a11f28f47cda7e561daed881f269b126c2bcd102
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-24 12:39:42 +0300

    Use array in the same line.

--- htdocs/wp-content/themes/syopajatyo/resources/views/partials/pagination-posts.php
+++ htdocs/wp-content/themes/syopajatyo/resources/views/partials/pagination-posts.php
@@ -5,10 +5,8 @@
  * @package Syopajatyo
  */
 
-the_posts_pagination(
-	[
-		'prev_text'          => Syopajatyo\get_svg( [ 'icon' => 'arrow-left' ] ) . '<span class="screen-reader-text">' . esc_html__( 'Previous page', 'syopajatyo' ) . '</span>',
-		'next_text'          => '<span class="screen-reader-text">' . esc_html__( 'Next page', 'syopajatyo' ) . '</span>' . Syopajatyo\get_svg( [ 'icon' => 'arrow-right' ] ),
-		'before_page_number' => '<span class="screen-reader-text">' . esc_html__( 'Page', 'syopajatyo' ) . ' </span>',
-	]
-);
+the_posts_pagination( [
+	'prev_text'          => Syopajatyo\get_svg( [ 'icon' => 'arrow-left' ] ) . '<span class="screen-reader-text">' . esc_html__( 'Previous page', 'syopajatyo' ) . '</span>',
+	'next_text'          => '<span class="screen-reader-text">' . esc_html__( 'Next page', 'syopajatyo' ) . '</span>' . Syopajatyo\get_svg( [ 'icon' => 'arrow-right' ] ),
+	'before_page_number' => '<span class="screen-reader-text">' . esc_html__( 'Page', 'syopajatyo' ) . ' </span>',
+] );
