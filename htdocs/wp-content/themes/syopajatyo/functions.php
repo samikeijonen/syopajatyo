2d02da9737e083c42076fc97b55de2f035d5632e
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-30 11:40:07 +0300

    Try using HC from root composer file.

--- htdocs/wp-content/themes/syopajatyo/functions.php
+++ htdocs/wp-content/themes/syopajatyo/functions.php
@@ -12,11 +12,9 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-// Auto-load any projects via the Composer autoloader. Be sure to check if the
-// file exists in case someone's using Composer to load their dependencies in
-// a different directory.
-if ( file_exists( get_parent_theme_file_path( 'vendor/autoload.php' ) ) ) {
-	require_once get_parent_theme_file_path( 'vendor/autoload.php' );
+// Load Hybrid Core.
+if ( file_exists( get_parent_theme_file_path( 'hybrid-core/src/bootstrap-hybrid.php' ) ) ) {
+	require_once get_parent_theme_file_path( 'hybrid-core/src/bootstrap-hybrid.php' );
 }
 
 // Bootstrap the theme. This loads any functions-files from the `/app` folder
