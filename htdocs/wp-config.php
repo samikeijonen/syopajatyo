84a15622c2960b94a11b3ab74d2ad1a36f18b8b1
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-30 21:00:07 +0300

    Remove error logs.

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -5,9 +5,7 @@
 ##### Read more at                                                               #####
 ##### https://seravo.com/docs/development/configure-vagrant-box/#using-dotenv    #####
 ######################################################################################
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 #Load composer libraries
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
