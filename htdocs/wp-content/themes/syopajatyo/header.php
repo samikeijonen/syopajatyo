4394243bcc24906923c0afc3b48b235166be9d34
Sami Keijonen <sami.keijonen@foxnet.fi>
2022-11-23 16:45:14 +0200

    Add meta info.

--- htdocs/wp-content/themes/syopajatyo/header.php
+++ htdocs/wp-content/themes/syopajatyo/header.php
@@ -12,6 +12,9 @@
 <html <?php language_attributes(); ?> class="no-js">
 
 <head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
